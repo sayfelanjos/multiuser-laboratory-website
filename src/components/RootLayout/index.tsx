9ab1cc2283562af99dbc24210964769c379af3c0
saymon <saymon.anjos@eng4tech.com.br>
2024-04-21 01:29:24 -0300

    change hero images and setup sizes

--- src/components/RootLayout/index.tsx
+++ src/components/RootLayout/index.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import Header from "../Header";
 import Footer from "../Footer";
 import Container from "react-bootstrap/Container";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <Container fluid className="p-0 position-relative">
-      <Header />
-      <Container fluid className="main-content">
-        <Outlet />
+    <>
+      <Container fluid className="p-0">
+        <Header />
+        <Container fluid className="main p-0 position-relative top-0 bottom-0">
+          <Outlet />
+        </Container>
+        <Footer />
       </Container>
-      <Footer />
-    </Container>
+      <ScrollRestoration />
+    </>
   );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Hero from "../../components/Hero";
-import Container from "react-bootstrap/Container";
 import CardsGrid from "../../components/CardsGrid";
 
 const Home = () => {
   return (
-    <Container fluid className="main-container p-0">
+    <>
       <Hero />
       <CardsGrid />
-    </Container>
+    </>
   );
 };
 
