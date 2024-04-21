79e2f9ece9ab22dfb4ef8a875f53177b6da32ea9
saymon <saymon.anjos@eng4tech.com.br>
2024-04-21 01:29:24 -0300

    change hero images and setup sizes

---
+++ functions/build/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "helloWorld", {
+  enumerable: true,
+  get: function () {
+    return _helloWorld.helloWorld;
+  }
+});
+var _helloWorld = require("./helloWorld");
