004835cbc53da2ee3d7c913fcd23ffb0195d8687
saymon <sayfelanjos@gmail.com>
2024-05-03 20:55:02 -0300

    update quote-request form

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -242,8 +242,8 @@ const Footer = () => {
       </Row>
       <Row xs={1} sm={1} md={1} lg={1} xl={1} xxl={1}>
         <p className="footer-copyright m-0 border-black border-top border-1 text-bg-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start">
-          Copyright &copy; {year} Faculdade de Engenharia Mecânica. All Rights
-          Reserved
+          Copyright &copy; {year} Faculdade de Engenharia Mecânica. Todos os
+          direitos reservados
         </p>
       </Row>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -36,6 +36,10 @@ const router = createBrowserRouter([
         path: "home",
         element: <Navigate to="/" />,
       },
+      // {
+      //   path: "schedule",
+      //   element: <SchedulePage />,
+      // },
       {
         path: "quote-request",
         element: <QuoteRequest />,
