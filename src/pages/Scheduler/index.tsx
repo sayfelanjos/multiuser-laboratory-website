69ba3f4549eaba00203f1ceca4be1d8cdef95970
saymon <sayfelanjos@gmail.com>
2024-11-04 23:06:59 -0300

    remove user manager to public users access

--- src/pages/Scheduler/index.tsx
+++ src/pages/Scheduler/index.tsx
@@ -3,57 +3,56 @@ import Container from "react-bootstrap/Container";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import "./_scheduler.scss";
-import { map } from "react-bootstrap/ElementChildren";
 
 const calendars = [
   {
     calendarKey: "0",
-    calendarName: "Análise Térmica e Reologia",
+    calendarTitle: "Análise Térmica e Reologia",
     calendarId: "efvtfrUCMceyZB4b6",
   },
   {
     calendarKey: "1",
-    calendarName: "Apóio Técnico",
+    calendarTitle: "Apóio Técnico",
     calendarId: "Mcp16sQXcyS8NcTu5",
   },
   {
     calendarKey: "2",
-    calendarName: "Charpy",
+    calendarTitle: "Charpy",
     calendarId: "CApvLZ7oBXSQ9zos6",
   },
   {
     calendarKey: "3",
-    calendarName: "Dureza, Microcopia Ótica e Metalografia",
+    calendarTitle: "Dureza e Metalografia",
     calendarId: "7wKZUQeeX4REKfVP7",
   },
   {
     calendarKey: "4",
-    calendarName: "FT-IR",
+    calendarTitle: "FT-IR",
     calendarId: "EWrgre5rd3aXjbN1A",
   },
   {
     calendarKey: "5",
-    calendarName: "Impressão 3D",
+    calendarTitle: "Impressão 3D",
     calendarId: "4Lxzw4hsfu9ec6vn6",
   },
   {
     calendarKey: "6",
-    calendarName: "MEV",
+    calendarTitle: "MEV",
     calendarId: "Wu7T1wmY9gHE961j8",
   },
   {
     calendarKey: "7",
-    calendarName: "MTS",
+    calendarTitle: "MTS",
     calendarId: "DkSLgKdF92DGGMBo6",
   },
   {
     calendarKey: "8",
-    calendarName: "Treinamento",
+    calendarTitle: "Treinamento",
     calendarId: "BQxHFrmnJ5FdyLhx6",
   },
   {
     calendarKey: "9",
-    calendarName: "Metalografia",
+    calendarTitle: "Microscopia Ótica",
     calendarId: "1DwCpfPmwMDDwpfh9",
   },
 ];
@@ -62,7 +61,7 @@ const Scheduler = () => {
   return (
     <Container className="h-100 p-2 overflow-y-auto">
       <Tabs
-        defaultActiveKey="mechanic-test01"
+        defaultActiveKey="0"
         transition
         id="noanim-tab-example"
         className="mb-3"
@@ -74,7 +73,7 @@ const Scheduler = () => {
               key={calendar.calendarKey}
               className="h-100"
               eventKey={calendar.calendarKey}
-              title={calendar.calendarName}
+              title={calendar.calendarTitle}
             >
               <Container className="calendar__ctn">
                 <iframe
