acedb925f7e384300d84e4f4cb985318ce29006f
saymon <sayfelanjos@gmail.com>
2025-06-19 17:14:56 -0300

    Match local and remote develop branch

--- src/firebase.ts
+++ src/firebase.ts
@@ -16,6 +16,8 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
+console.log(process.env.REACT_APP_FIREBASE_PROJECT_ID);
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
