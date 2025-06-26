0aa216a344b3e2b52dbd53a29ea014e678fadfb1
Rafael Ogallas <rafaelogallas@gmail.com>
2025-06-26 01:05:54 -0300

    Updated footer links, fixes #12
    Updated footer links according to described in the Trello task:
     - https://trello.com/c/sHdIO6Ra
    
    Also, as the number of links decreased significantly for the 2 first
    columns, adjusted the layout of the links as follows:
     - Changed media links direction to row.
     - Adjusted the columns size for larger screens to avoid disharmonious
       wrapping of text.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -20,32 +20,32 @@ const Footer = () => {
           xs={{ span: 12, offset: 0 }}
           sm={{ span: 12, offset: 0 }}
           md={{ span: 5, offset: 1 }}
-          lg={{ span: 3, offset: 1 }}
-          xl={{ span: 3, offset: 1 }}
-          xxl={{ span: 3, offset: 1 }}
+          lg={{ span: 4, offset: 1 }}
+          xl={{ span: 4, offset: 1 }}
+          xxl={{ span: 4, offset: 1 }}
         >
           <Stack
             gap={1}
             className={
               "pb-3 " +
               "align-items-center " +
-              "align-items-sm-center " +
-              "align-items-md-start " +
-              "align-items-lg-start " +
-              "align-items-xl-start " +
-              "align-items-xxl-start " +
-              "flex-column " +
-              "flex-sm-column " +
-              "flex-md-column " +
-              "flex-lg-column " +
-              "flex-xl-column " +
-              "flex-xxl-column"
+              // "align-items-sm-center " +
+              // "align-items-md-start " +
+              // "align-items-lg-start " +
+              // "align-items-xl-start " +
+              // "align-items-xxl-start " +
+              "flex-column " // +
+              // "flex-sm-column " +
+              // "flex-md-column " +
+              // "flex-lg-column " +
+              // "flex-xl-column " +
+              // "flex-xxl-column"
             }
           >
             <h6 className="text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start navbar-text text-light">
               Acesso Rápido
             </h6>
-            <Link
+            {/* <Link
               to="http://www.fem.unicamp.br/index.php/pt-br/institucional/diretoria"
               className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
             >
@@ -56,14 +56,16 @@ const Footer = () => {
               className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
             >
               Departamento de Energia
-            </Link>
+            </Link> */}
+
             <Link
               to="http://www.fem.unicamp.br/index.php/pt-br/demm-sobrenos"
               className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
             >
               Departamento de Engenharia de Manufatura e Materiais
             </Link>
-            <Link
+
+            {/* <Link
               to="http://www.fem.unicamp.br/index.php/pt-br/dmc-sobrenos"
               className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
             >
@@ -81,33 +83,33 @@ const Footer = () => {
             >
               POLÍTICA DE SEGURANÇA DA INFORMAÇÃO DA UNICAMP (Deliberação
               CONSU-A-031/2020, de 04/08/2020)
-            </Link>
+            </Link> */}
           </Stack>
         </Col>
         <Col
           xs={{ span: 12, offset: 0 }}
           sm={{ span: 12, offset: 0 }}
           md={{ span: 5, offset: 1 }}
-          lg={{ span: 3, offset: 1 }}
-          xl={{ span: 3, offset: 1 }}
-          xxl={{ span: 3, offset: 1 }}
+          lg={{ span: 4, offset: 0 }}
+          xl={{ span: 4, offset: 0 }}
+          xxl={{ span: 4, offset: 0 }}
         >
           <Stack
             gap={1}
             className={
               "pb-3 " +
               "align-items-center " +
-              "align-items-sm-center " +
-              "align-items-md-start " +
-              "align-items-lg-start " +
-              "align-items-xl-start " +
-              "align-items-xxl-start " +
-              "flex-column " +
-              "flex-sm-column " +
-              "flex-md-column " +
-              "flex-lg-column " +
-              "flex-xl-column " +
-              "flex-xxl-column"
+              // "align-items-sm-center " +
+              // "align-items-md-start " +
+              // "align-items-lg-start " +
+              // "align-items-xl-start " +
+              // "align-items-xxl-start " +
+              "flex-column " // +
+              // "flex-sm-column " +
+              // "flex-md-column " +
+              // "flex-lg-column " +
+              // "flex-xl-column " +
+              // "flex-xxl-column"
             }
           >
             <h6 className="text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start navbar-text text-light">
@@ -119,7 +121,8 @@ const Footer = () => {
             >
               Biblioteca da Área de Engenharia - BAE
             </Link>
-            <Link
+
+            { /* <Link
               to="http://www.dac.unicamp.br/"
               className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
             >
@@ -130,14 +133,16 @@ const Footer = () => {
               className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
             >
               Serviço de Apoio ao Estudante - SAE
-            </Link>
+            </Link> */ }
+
             <Link
               to="http://www.sbu.unicamp.br/"
               className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
             >
               Sistema de Bibliotecas da Unicamp - SBU
             </Link>
-            <Link
+
+            { /* <Link
               to="http://www.unicamp.br/sipex/"
               className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
             >
@@ -193,18 +198,19 @@ const Footer = () => {
               className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start "
             >
               Prefeitura da Unicamp
-            </Link>
+            </Link> */ }
+            
           </Stack>
         </Col>
         <Col
           xs={{ span: 12, offset: 0 }}
           sm={{ span: 12, offset: 0 }}
           md={{ span: 12, offset: 0 }}
-          lg={{ span: 3, offset: 1 }}
-          xl={{ span: 3, offset: 1 }}
-          xxl={{ span: 3, offset: 1 }}
+          lg={{ span: 3, offset: 0 }}
+          xl={{ span: 3, offset: 0 }}
+          xxl={{ span: 3, offset: 0 }}
         >
-          <h6 className="text-center text-md-centertext-lg-start text-xl-start text-xxl-start py text-lg-start mb-3 navbar-text text-light">
+          <h6 className="text-center mb-3 navbar-text text-light">
             Mídias Sociais
           </h6>
           <Stack
@@ -212,17 +218,18 @@ const Footer = () => {
             className={
               "pb-3 " +
               "justify-content-center " +
-              "justify-content-sm-center " +
-              "justify-content-md-center " +
-              "align-items-lg-start " +
-              "align-items-xl-start " +
-              "align-items-xxl-start " +
-              "flex-row " +
-              "flex-sm-row " +
-              "flex-md-row " +
-              "flex-lg-column " +
-              "flex-xl-column " +
-              "flex-xxl-column"
+              // "justify-content-sm-center " +
+              // "justify-content-md-center " +
+              "align-items-center " +
+              // "align-items-lg-start " +
+              // "align-items-xl-start " +
+              // "align-items-xxl-start " +
+              "flex-row "
+              // "flex-sm-row " +
+              // "flex-md-row " + 
+              // "flex-lg-column " +
+              // "flex-xl-column " +
+              // "flex-xxl-column"
             }
           >
             <Link
