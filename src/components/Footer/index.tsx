dd807fa366bc42f98662dd9e03e8e6eb2eba0803
Rafael Ogallas <rafaelogallas@gmail.com>
2025-08-05 22:48:38 -0300

    refactor(footer): extract data and replace Link
    - Moved quick access, other sites, and media links to separate arrays
      for better maintainability and scalability.
    - Replaced the Link component with native <a> tags to ensure proper
      behavior for external URLs and improve accessibility.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { ComponentType } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Stack from "react-bootstrap/Stack";
-import FacebookIcon from "../../assets/icons/FacebookIcon";
-import { Link } from "react-router-dom";
-import LinkedInIcon from "../../assets/icons/LinkedInIcon";
-import WhatsAppIcon from "../../assets/icons/WhatsAppIcon";
-import InstagramIcon from "../../assets/icons/InstagramIcon";
+import { quickAccessLinks, otherLinks, mediaLinks } from "./linksData";
 
 const d = new Date();
 const year = d.getFullYear();
+
+const LinkText = ({ url, text }: { url: string; text: string }) => (
+  <a
+    href={url}
+    className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
+  >
+    {text}
+  </a>
+);
+
+const LinkIcon = ({ url, Icon }: { url: string; Icon: ComponentType }) => (
+  <a href={url} className="link-light icon-link-hover">
+    <Icon />
+  </a>
+);
 
 const Footer = () => {
   return (
@@ -24,66 +35,14 @@ const Footer = () => {
           xl={{ span: 4, offset: 1 }}
           xxl={{ span: 4, offset: 1 }}
         >
-          <Stack
-            gap={1}
-            className={
-              "pb-3 " +
-              "align-items-center " +
-              // "align-items-sm-center " +
-              // "align-items-md-start " +
-              // "align-items-lg-start " +
-              // "align-items-xl-start " +
-              // "align-items-xxl-start " +
-              "flex-column " // +
-              // "flex-sm-column " +
-              // "flex-md-column " +
-              // "flex-lg-column " +
-              // "flex-xl-column " +
-              // "flex-xxl-column"
-            }
-          >
+          <Stack gap={1} className={"pb-3 " + "align-items-center "}>
             <h6 className="text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start navbar-text text-light">
               Acesso Rápido
             </h6>
-            {/* <Link
-              to="http://www.fem.unicamp.br/index.php/pt-br/institucional/diretoria"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              Diretoria
-            </Link>
-            <Link
-              to="http://www.fem.unicamp.br/index.php/pt-br/de-sobrenos"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              Departamento de Energia
-            </Link> */}
 
-            <Link
-              to="http://www.fem.unicamp.br/index.php/pt-br/demm-sobrenos"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              Departamento de Engenharia de Manufatura e Materiais
-            </Link>
-
-            {/* <Link
-              to="http://www.fem.unicamp.br/index.php/pt-br/dmc-sobrenos"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              Departamento de Mecânica Computacional
-            </Link>
-            <Link
-              to="http://www.fem.unicamp.br/index.php/pt-br/dsi-sobrenos"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              Departamento de Sistemas Integrados
-            </Link>
-            <Link
-              to="http://www.pg.unicamp.br/mostra_norma.php?id_norma=22826"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              POLÍTICA DE SEGURANÇA DA INFORMAÇÃO DA UNICAMP (Deliberação
-              CONSU-A-031/2020, de 04/08/2020)
-            </Link> */}
+            {quickAccessLinks.map((link, index) => (
+              <LinkText key={index} url={link.url} text={link.text} />
+            ))}
           </Stack>
         </Col>
         <Col
@@ -96,111 +55,15 @@ const Footer = () => {
         >
           <Stack
             gap={1}
-            className={
-              "pb-3 " +
-              "align-items-center " +
-              // "align-items-sm-center " +
-              // "align-items-md-start " +
-              // "align-items-lg-start " +
-              // "align-items-xl-start " +
-              // "align-items-xxl-start " +
-              "flex-column " // +
-              // "flex-sm-column " +
-              // "flex-md-column " +
-              // "flex-lg-column " +
-              // "flex-xl-column " +
-              // "flex-xxl-column"
-            }
+            className={"pb-3 " + "align-items-center " + "flex-column "}
           >
             <h6 className="text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start navbar-text text-light">
               Outros Sites
             </h6>
-            <Link
-              to="https://www.bae.unicamp.br/"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              Biblioteca da Área de Engenharia - BAE
-            </Link>
 
-            { /* <Link
-              to="http://www.dac.unicamp.br/"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              Diretoria Acadêmica - DAC
-            </Link>
-            <Link
-              to="http://www.sae.unicamp.br/"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              Serviço de Apoio ao Estudante - SAE
-            </Link> */ }
-
-            <Link
-              to="http://www.sbu.unicamp.br/"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              Sistema de Bibliotecas da Unicamp - SBU
-            </Link>
-            
-            <Link
-              to="http://www.unicamp.br/sipex/"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              Sistema de Informação de Pesquisa - SiPEx
-            </Link>
-
-            { /* <span className="d-inline">
-              <Link
-                to="http://www.capes.gov.br/"
-                className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-              >
-                Fundação Capes
-              </Link>{" "}
-              |
-              <Link
-                to="http://www.cnpq.br/"
-                className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-              >
-                {" "}
-                CNPq
-              </Link>{" "}
-              |
-              <Link
-                to="http://www.fapesp.br/"
-                className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-              >
-                {" "}
-                Fapesp
-              </Link>{" "}
-              |
-              <Link
-                to="http://www.finep.gov.br/"
-                className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-              >
-                {" "}
-                Finep
-              </Link>{" "}
-              |
-            </span>
-            <Link
-              to="https://www.ggte.unicamp.br/"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              Ensino Aberto Unicamp
-            </Link>
-            <Link
-              to="http://www.ccuec.unicamp.br/ccuec/rede_sem_fio"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start"
-            >
-              Rede sem Fio Unicamp - Eduroam
-            </Link>
-            <Link
-              to="http://www.prefeitura.unicamp.br/"
-              className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-light text-center text-sm-center text-md-start text-lg-start text-xl-start text-xxl-start "
-            >
-              Prefeitura da Unicamp
-            </Link> */ }
-            
+            {otherLinks.map((link, index) => (
+              <LinkText key={index} url={link.url} text={link.text} />
+            ))}
           </Stack>
         </Col>
         <Col
@@ -219,41 +82,13 @@ const Footer = () => {
             className={
               "pb-3 " +
               "justify-content-center " +
-              // "justify-content-sm-center " +
-              // "justify-content-md-center " +
               "align-items-center " +
-              // "align-items-lg-start " +
-              // "align-items-xl-start " +
-              // "align-items-xxl-start " +
               "flex-row "
-              // "flex-sm-row " +
-              // "flex-md-row " + 
-              // "flex-lg-column " +
-              // "flex-xl-column " +
-              // "flex-xxl-column"
             }
           >
-            <Link
-              to="https://www.instagram.com/lmudemm/"
-              className="link-light icon-link-hover"
-            >
-              <InstagramIcon />
-            </Link>
-            <Link
-              to="https://www.linkedin.com/in/lmufemunicamp/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-              className="link-light icon-link-hover"
-            >
-              <LinkedInIcon />
-            </Link>
-            <Link to="#" className="link-light icon-link-hover">
-              <FacebookIcon />
-            </Link>
-            <Link
-              to="https://wa.me/qr/CGPC3MLVEXLBH1"
-              className="link-light icon-link-hover"
-            >
-              <WhatsAppIcon />
-            </Link>
+            {mediaLinks.map((link, index) => (
+              <LinkIcon key={index} url={link.url} Icon={link.Icon} />
+            ))}
           </Stack>
         </Col>
       </Row>
